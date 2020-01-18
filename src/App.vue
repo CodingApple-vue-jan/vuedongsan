2c05bd4779d993e04bf0b2bdf0c8fb39e05c5282
Hyungjun Rho <zuntopia@gmail.com>
2020-01-18 14:49:40 +0900

    bind, for

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <p v-bind:style="style"> {{ hello }}</p>
+    <!-- <h4 v-for="val in [1,2,3,4,5]" :key="val">{{val}} </h4> -->
+    <div v-for="post in posts" :key="post">
+      <h4> {{post.title}} </h4>
+      <p> {{post.date}} </p>
+    </div>
+    <h4>title</h4>
+    <p>date</p>
+    <h4>title</h4>
+    <p>date</p>
+    <h4>title</h4>
+    <p>date</p>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+  data(){
+    return {
+      hello: "world",
+      style: 'color: red;',
+      posts : [{
+          title : '오늘의 일기',
+          date : '4월 20일'
+        }, {
+          title : '수분크림 추천',
+          date : '3월 4일'
+        }, {
+          title : '강남 맛집 후기',
+          date : '2월 22일'
+        }
+      ],
+    }
+  },
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
