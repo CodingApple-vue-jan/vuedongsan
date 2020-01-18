c202f40948744e1107de30d94f74c896fd720d76
Hyungjun Rho <zuntopia@gmail.com>
2020-01-18 15:22:45 +0900

    using only bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"/> -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card" style="width: 18rem;">
+            <img src="./assets/logo.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">item2</div>
+      </div>
+    </div>
   </div>
 </template>
 
