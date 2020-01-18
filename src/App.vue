e8303ec08d2dbec7398520f76dbd4de55e63fc52
Hyungjun Rho <zuntopia@gmail.com>
2020-01-18 16:19:46 +0900

    data injection into components

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,7 @@
     <img alt="Vue logo" src="./assets/logo.png"/>
     <div class="container">
       <div class="row">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        <Card v-for="item in data" v-bind:item="item" :key=item></Card>
       </div>
     </div>
   </div>
@@ -17,24 +12,13 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import Card from './components/Card.vue'
+import data from './assets/data.js'
 
 export default {
   name: 'app',
   data(){
     return {
-      hello: "world",
-      style: 'color: red;',
-      posts : [{
-          title : '오늘의 일기',
-          date : '4월 20일'
-        }, {
-          title : '수분크림 추천',
-          date : '3월 4일'
-        }, {
-          title : '강남 맛집 후기',
-          date : '2월 22일'
-        }
-      ],
+      data: data,
     }
   },
   components: {
